68495120e880fa9f8e11008995090d5555f1c7ce
syahmi <syahmisyihabuddin44@gmail.com>
2026-04-16 19:52:32 +0800

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,26 +11,55 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.purple,
+        backgroundColor: Colors.deepPurpleAccent,
         body: SafeArea(
-          child: Container(
-            height: 220,
-            width:  220,
-            margin: EdgeInsets.symmetric(
-              vertical: 50,
-              horizontal: 10,
-            ),
-            color: Colors.white,
-            child: Text(
-              'SYAHMI',
-              style: TextStyle(fontSize: 50),
-          
+          child: Column(
+            children: [
+              CircleAvatar(
+              radius: 200,
+                backgroundImage: AssetImage('images/scemer-staring-cat.gif'),
+          ),
+              Container(
+                height: 90,
+                width:  500,
+                margin: EdgeInsets.symmetric(
+                  vertical: 50,
+                  horizontal: 10,
+                ),
+                color: Colors.blue,
+                  child: Center(
+                    child: Text(
+                      'Muhammad Syahmi Syihabuddin bin Syamsul Bahari',
+                      style: TextStyle(
+                        fontFamily: 'Pacifico',
+                          fontSize: 20),
+                    ),
+                  ),
+                ),
+              Icon(Icons.face),
+              Container(
+                height: 90,
+                width:  500,
+                margin: EdgeInsets.symmetric(
+                  vertical: 50,
+                  horizontal: 10,
+                ),
+                color: Colors.yellow,
+                child: Center(
+                  child: Text(
+                    'SYAHMI',
+                    style: TextStyle(
+                        fontFamily: 'Saira Stencil',
+                        fontSize: 40),
+                  ),
+                ),
+              ),
+            ],
             ),
           ),
         ),
-      ),
-    );
+      );
+    }
   }
-}
 
 
